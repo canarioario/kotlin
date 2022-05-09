9f2207bcfc878ee2440a067a2b36bed27f016388
canarioario <cucutequiero2@gmail.com>
2022-05-09 18:45:42 +0200

    geolocalizacion

---
+++ Tarea4Online/Geolocalizacion/Mapa/app/src/main/java/com/example/mapa/MapsActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mapa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.mapa.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val sydney = LatLng(37.26397453830096, -6.942979156330818)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Mi Lugar de estudio"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
